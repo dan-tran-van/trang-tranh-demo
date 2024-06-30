62f75ddf2c0fcef8120b24dfd5e499fea3d783a2
Dan Tran <tranvandanmail3@gmail.com>
2024-06-30 21:31:37 +0700

    Remove sesitive email info

--- trangtranhdemo/settings.py
+++ trangtranhdemo/settings.py
@@ -175,9 +175,3 @@ LOGIN_REDIRECT_URL = '/'
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend' 
 
-EMAIL_HOST = "smtp.gmail.com"
-EMAIL_HOST_USER = "tranvandanmail3@gmail.com"
-EMAIL_HOST_PASSWORD = "iiqg znbf unlo qnio"
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-DEFAULT_FROM_EMAIL = "Đàn tranvandanmail3@gmail.com"
